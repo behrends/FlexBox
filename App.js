37ab60e4bff8fcd4b1826e2feb2424916f716f0a
Erik Behrends <behrends@users.noreply.github.com>
2018-11-09 12:33:01 +0100

    Höhe und Breite mit absoluten Werten festlegen

--- App.js
+++ App.js
@@ -5,12 +5,31 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View />
+        <View style={styles.square} />
+        <View style={styles.box} />
+        <View style={styles.boxUp} />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    paddingTop: 100
+  },
+  square: {
+    width: 150,
+    height: 150,
+    backgroundColor: 'cyan'
+  },
+  box: {
+    width: 350,
+    height: 100,
+    backgroundColor: 'magenta'
+  },
+  boxUp: {
+    width: 100,
+    height: 250,
+    backgroundColor: 'yellow'
+  }
 });
