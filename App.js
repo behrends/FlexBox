37ead08825ec22a5ffd62cc748709220f722af93
Erik Behrends <behrends@users.noreply.github.com>
2018-11-09 11:25:59 +0100

    Projekt erstellt und vorbereitet

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
